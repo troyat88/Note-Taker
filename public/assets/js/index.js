40b26094886efe1f5233fe9091112131e0167fa3
Troy <troyat88@gmail.com>
2021-08-31 00:04:49 -0700

    created paths, need to work on showing selected

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -181,3 +181,5 @@ if (window.location.pathname === '/notes') {
 }
 
 getAndRenderNotes();
+
+module.exports = getAndRenderNotes;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express =  require("express");
 const path = require("path");
 const fs = require("fs");
+const uniqid = require("uniqid")
+
+
 
 var app = express();
 var PORT = process.env.PORT || 3001;
@@ -10,13 +13,59 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => 
-  res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
+//Route for Homepage
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
+//Route for NotesPage
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname,'.'))
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
+
+//Displays notes in sidebar 
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+
+//Post new Note
+app.post('/api/notes', (req, res) => {
+ 
+  const { title, text} = req.body;
+
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      Id: uniqid(),
+    };
+    //fs.writeFileSync("db/db.json",JSON.stringify([newNote]), (err) =>
+    //err ? console.error(err) : console.info(`Success`));
+    
+    const notesData = JSON.parse(fs.readFileSync("db/db.json"))
+    notesData.push(newNote)
+
+    fs.writeFileSync("db/db.json",JSON.stringify(notesData, null,2))
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
+
+app.get("/api/notes/:note", function (req, res) {
+  var selectedNote = req.params.note;
+  res.json(selectedNote);
+});
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`))
